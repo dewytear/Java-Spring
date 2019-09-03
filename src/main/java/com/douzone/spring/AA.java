7ca23b197c4e2d98071c78980ceae8c06080db5b
dewytear <youngjinkwak@gmail.com>
2019-09-03 16:08:44 +0900

    20190903 string

---
+++ src/main/java/com/douzone/spring/AA.java
@@ -0,0 +1,11 @@
+package com.douzone.spring;
+
+public class AA {
+
+    BB bb = new BB();
+    
+    public void print(){
+        bb.aa();
+    }
+    
+}
---
+++ src/main/java/com/douzone/spring/BB.java
@@ -0,0 +1,9 @@
+package com.douzone.spring;
+
+public class BB {
+
+    public void aa(){
+        System.out.println("BB객체의 aa() 메소드 입니다!!!");
+    }
+	
+}
